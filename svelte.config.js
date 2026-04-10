8af15105f390a0f5048c7c63ea91f6d3b07c0388
lelesf99 <viniciuslelesf@gmail.com>
2026-04-10 13:28:15 -0300

    fixed reponame

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-const repoName = 'calcbook-js';
+const repoName = 'Calcbook-JS';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
